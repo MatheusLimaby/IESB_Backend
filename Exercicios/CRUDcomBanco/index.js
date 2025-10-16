c511e3d1a22202c6b12998c5c5b72e8898b8b375
MatheusLimaby <matheus.l.rodrigues@iesb.edu.br>
2025-10-16 20:27:59 -0300

    teste

---
+++ Exercicios/CRUDcomBanco/index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+app.use(express.json())
+
+// Connetar no banco Mongo
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_NAME = process.env.DB_NAME
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connect(url)
+  .then(() => {
+    console.log("Conectado ao MongoDB")
+  })
+  .catch(err => {
+    console.log("Erro ao conectar no banco MongoDB: ", err)
+  })
+
+// Interface com o banco de dados - Model
+// Cada model(Modelo) representa uma Collection(Tabela)
+const LivroModel = mongoose.model('Livros', new mongoose.Schema(
+  {
+    titulo: String,
+    autor: String,
+    editora: String,
+    ano: Number,
+    preco: Number
+  }
+))
+
+// CRUD
+
+// Create
+app.post('/livros', async (req, res, next) => {
+  const livro = req.body
+  if (!tarefa.nome) {
+    return res.status(400).json({ erro: "O campo nome é obrigatório!!!" })
+  }
+  const tarefaCriada = await TarefaModel.create(tarefa)
+  res.status(201).json(tarefaCriada)
+})
+
+// READ
+app.get('/livros', async (req, res, next) => {
+  const livros = await TarefaModel.find()
+  res.json(tarefas)
+})
+
+// UPDATE
+app.put('/livros/:id', async (req, res, next) => {
+  const id = req.params.id
+  const livro = req.body
+  if (!livro.titulo || !livro.autor || !livro.editora || !livro.ano || !livro.preco) {
+    return res.status(400).json({ erro: "O campo nome é obrigatório!!!" })
+  }
+
+  const livroAtualizado = await livroModel.findByIdAndUpdate(id, livro, { new: true })
+
+  if (!livroAtualizado) {
+    return res.status(404).json({ erro: "livro não encontrado!!!" })
+  }
+  res.json(livroAtualizado)
+})
+
+// DELETE
+app.delete('/livros/:id', async (req, res, next) => {
+  const id = req.params.id
+  await livroModel.findByIdAndDelete(id)
+  res.json({ mensagem: "livro excluido!!!" })
+})
+
+// start
+app.listen(3000, () => {
+  console.log("Aplicação rodando em http://localhost:3000")
+})
